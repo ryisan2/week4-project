849e21415f89f0a00adc10e7c02aa9d04aef640b
rysian2 <ryisan2@gmail.com>
2024-05-04 00:11:51 -0400

    new button

--- email.js
+++ email.js
@@ -5,5 +5,6 @@
   
         function contact (event) {
             event.preventDefault()
+            emailjs.sendForm('service_nqzn54c', 'template_jn7jfdq', event.target, 'user_LwKeSJJC1tZeCru0b')
             console.log("it works.")
         }
